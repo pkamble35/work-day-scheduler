b59b635cbf524e2fc019ec254f1d57c702cda38c
priyanka damodar <priyadamodar12@gmail.com>
2021-02-23 22:37:46 -0500

    implemented scheduler

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    function initialize() {
+        $('#currentDay').text(moment().format('LLL'));
+        var eventList = $('.description');
+        var currentTime = moment().hour();
+        eventList.each(function( index ) {
+            var timeBlock = $( this ).parent().attr('time-attr');
+            if(localStorage.getItem(timeBlock)){
+                $( this ).html( localStorage.getItem(timeBlock));
+            }
+            if(timeBlock == currentTime){
+                $( this ).addClass('current');
+            }
+            else if (timeBlock < currentTime) {
+                $( this ).addClass('past');
+            } else {
+                $( this ).addClass('future');
+            }
+          });
+        
+    }
+    $('.saveBtn').on("click", function() {
+        var parentValue = $( this ).parent();
+        var value = parentValue.find('.description').val();        
+        localStorage.setItem(parentValue.attr('time-attr'), value);
+      });
+
+    initialize();
+});
